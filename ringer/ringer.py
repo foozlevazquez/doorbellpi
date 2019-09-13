3efdd2b9e83799f4cdfd352c02fe02b88ca300d5
Ivan Vazquez <ivan@ivanvazquez.com>
2019-09-12 22:10:23 -0700

    Added pics.

--- ringer/ringer.py
+++ ringer/ringer.py
@@ -5,21 +5,29 @@ import logging
 import threading
 import subprocess
 
+import requests
+
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 
 
 os.sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)),
                                 '../lib/py'))
 
-
 from mq import RabbitMQ
 
 
 def handle_message(ch, method, properties, body):
     print(" received: {}".format(body))
+
     if 'pressed' in body.decode('utf-8'):
         logging.info("Playing...")
         subprocess.run(["/usr/bin/aplay", "/home/pi/ChurchTowerClock.wav"])
+
+        # Save front door image
+        dstr = str(datetime.utcnow()).replace(' ', '_')
+        r = requests.get('http://192.168.110.34/image/jpeg.cgi')
+        with open('/opt/doorbell_pics/{}.jpg'.format(dstr), 'wb') as f:
+            f.write(r.content)
 
 
 if __name__ == '__main__':
